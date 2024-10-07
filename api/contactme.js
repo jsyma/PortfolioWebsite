8aabb3c9e3350122724b6f86ebc0929a66426b40
jsyma <johnnyma99@gmail.com>
2024-10-06 23:11:12 -0400

    add endpoint for email submissions

---
+++ api/contactme.js
@@ -0,0 +1,36 @@
+import sgMail from '@sendgrid/mail';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+export default async function handler(req, res) {
+    console.log("Incoming request:", req.body); 
+  if (req.method === 'POST') {
+    const { name, email, subject, message } = req.body;
+
+    const msg = {
+      to: process.env.SENDGRID_TO_EMAIL,
+      from: process.env.SENDGRID_FROM_EMAIL,
+      templateId: process.env.SENDGRID_TEMPLATE_ID,
+      dynamic_template_data: {
+        name,
+        email,
+        subject,
+        message,
+      },
+    };
+
+    try {
+      await sgMail.send(msg);
+      return res.status(200).json({ success: true, message: 'Email sent successfully!' });
+    } catch (error) {
+      console.error('Error sending email:', error);
+      return res.status(500).json({ error: 'Error sending email' });
+    }
+  } else {
+    res.setHeader('Allow', ['POST']);
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+}
